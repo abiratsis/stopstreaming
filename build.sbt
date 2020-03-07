e54fa25c169c35ec4f47cdfc4d8d41dd0a18d397
Alexandros Biratsis <>
2020-03-07 23:34:06 +0100

    first commit

---
+++ build.sbt
@@ -0,0 +1,7 @@
+name := "StopStreamingGracefully"
+
+version := "0.1"
+
+scalaVersion := "2.13.1"
+
+
---
+++ src/main/scala/com/abiratsis/spark/streaming/stopper/Main.scala
@@ -0,0 +1,9 @@
+package com.abiratsis.spark.streaming.stopper
+
+object Main {
+
+  def main(args: Array[String]): Unit = {
+    println("Hello from main")
+  }
+
+}
